c7ff9e896773cfde1176f4cfb2374c25646c5afe
Jazz-Zealot <py.cai@outlook.com>
2025-03-31 01:01:22 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,4 @@
-
-const sentences = [
-  {
-    id: 1,
-    sentence: "After six months of arguing and final 16 hours of hot parliamentary debates, Australia’s Northern Territory became the first legal authority in the world to allow doctors to take the lives of incurably ill patients who wish to die.",
-    parts: [
-      "After six months of arguing,",
-      "and final 16 hours of hot parliamentary debates,",
-      "Australia’s Northern Territory became the first legal authority in the world,",
-      "to allow doctors to take the lives of incurably ill patients who wish to die."
-    ],
-    audio: "audio/D1.mp3",
-    image: "images/sentence1.png"
-  },
-  {
-    id: 2,
-    sentence: "The casual friendliness of many Americans should be interpreted neither as superficial nor as artificial, but as the result of a historically developed cultural tradition.",
-    parts: [
-      "The casual friendliness of many Americans",
-      "should be interpreted neither as superficial,",
-      "nor as artificial,",
-      "but as the result of a historically developed cultural tradition."
-    ],
-    audio: "audio/D2.mp3",
-    image: "images/sentence2.png"
-  },
-  {
-    id: 3,
-    sentence: "For example, when an American uses the word ‘friend’, the cultural implications of the word may be quite different from those it has in the visitor’s language and culture.",
-    parts: [
-      "For example,",
-      "when an American uses the word ‘friend’,",
-      "the cultural implications of the word may be quite different",
-      "from those it has in the visitor’s language and culture."
-    ],
-    audio: "audio/D3.mp3",
-    image: "images/sentence3.png"
-  }
-];
+import sentences from './data.js';
 
 let currentIndex = -1;
 let score = parseInt(localStorage.getItem("score")) || 0;
